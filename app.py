c45c85db8842ff90367e88440747f037753a3dcd
amitagrawalproject <amit8work@outlook.com>
2025-04-08 09:59:47 +0530

    creating db tables

--- app.py
+++ app.py
@@ -1,53 +1,35 @@
-import uuid
+import os
 
-from flask import Flask,request
-from db import stores,items
-
-app = Flask(__name__)
-
-
-@app.get("/store") #http://127.0.0.1:5000/store
-def get_stores():
-    return {"stores":list(stores.values())}
+from flask import Flask
+from flask_smorest import Api
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from db import db
+import models
 
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4.hex
-    store = {**store_data,"id":store_id}
-    stores[store_id] = store
-    return store, 201
+def create_app(db_url=None):
+    app = Flask(__name__)
 
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        return {"message":"store not found"}, 404
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores Rest API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)  #connecting sqlalchemy with flask app
 
+    api = Api(app)
 
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message":"store not found"}, 404
+    @app.before_request
+    def create_tables():
+        db.create_all()
 
-    item_id = uuid.uuid4().hex
-    item = {**item_data,"id":item_id}
-    item[item_id] = item
-    return item, 201
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
-
-@app.get("/item")
-def get_all_items():
-    return {"items":list(items.values())}
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"Item not found"}, 404
+    return app
--- resources/item.py
+++ resources/item.py
@@ -2,7 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description= "Operations on items")
@@ -12,48 +14,64 @@ blp = Blueprint("Items", __name__, description= "Operations on items")
 class ItemList(MethodView):
     @blp.response(200,ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
 
+    # @blp.arguments(ItemSchema) - This decorator tells Flask to:Parse the request body using the ItemSchema,
+    # Validate the input (based on required=True,
+    # types, etc.),Deserialize the JSON payload into a Python dictionary
+    # Instead of manually calling request.get_json(),
+    # you get item_data as a clean Python dictionary with validated fields
     @blp.arguments(ItemSchema)
     @blp.response(200,ItemSchema)
-    def post(self,item_data):
-        for item in items.values():
-            if item_data["name"] == item["name"] or item_data["store_id"] == item["store_id"]:
-                abort(400, message="item already exists.")
+    # @blp.response(200, ItemSchema)
+    # Purpose: This sets the expected response schema and status code.
+    #
+    # It tells Flask to:
+    #
+    # Serialize the return value (usually a dictionary) using ItemSchema
+    #
+    # Return a 200 OK response
+    #
+    # Auto-generate Swagger documentation for it too!
+    def post(self,item_data):  # item_data is passed by @blp.arguments — it’s your validated request data.
+        item = ItemModel(**item_data)
 
-        # if item_data["store_id"] not in stores:
-        #     abort(404, message="store not found.")
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message= "An error occurred while inserting the item")
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-        return item, 201
+        return item
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
     @blp.response(200,ItemSchema)
     def get(self,item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200,ItemSchema)
     def put(self,item_data,item_id):
-        try:
-            item = items[item_id]
-            item.update(item_data)
-            return item
-        except KeyError:
-            abort(404, message="item not found.")
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id = item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+        return item
 
     def delete(self,item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted"}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted"}
+
+
 
 
