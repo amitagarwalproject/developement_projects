6d117ffe2c515d0b208670b30005a46feac40191
amitagrawalproject <amit8work@outlook.com>
2025-04-11 23:15:33 +0530

    added user registration endpoint

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 from db import db
-import models
+from flask_jwt_extended import JWTManager
 
 
 def create_app(db_url=None):
     app = Flask(__name__)
-
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores Rest API"
@@ -23,6 +23,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)  # connecting sqlalchemy with flask app
+    app.config["JWT_SECRET_KEY"] = "jose"
 
     api = Api(app) # initializing the Flask-Smorest Api object and binding it to your Flask app.
     '''It wraps your Flask app and adds support for:
@@ -35,6 +36,8 @@ def create_app(db_url=None):
 
     📦 Input parsing and output formatting made easy.'''
 
+    jwt = JWTManager(app)
+
     @app.before_request
     def create_tables():
         db.create_all()
@@ -42,5 +45,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
--- schemas.py
+++ schemas.py
@@ -25,6 +25,7 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True) # load_only=True: This field is used only when receiving data, not returned in responses
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema(), dump_only=True))
 
 
 class StoreSchema(PlainStoreSchema):
@@ -35,6 +36,26 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+
+
+'''dump_only = True - This field will only appear in the response and not required or allowed in the request 
+load_only = True - You want to accept some data in input (like a password) But you don’t want to show it in the 
+output (for security) required = True - required in the request data'''
+
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+
+
 
 
 
